3b829b3d4ad5774b95cc1ed5333d9a66bac4c72f
clement <clems.corsica70@gmail.com>
2023-07-06 18:19:28 +0200

    index php route

--- public/index.php
+++ public/index.php
@@ -7,3 +7,19 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
+
+if (getenv('JAWSDB_URL') !== false){
+    $dbparts = parse_url(getenv('JAWSDB_URL'));
+    
+    $hostname = $dbparts['host'];
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
+    $database = ltrim($dbparts['path'],'/');
+} else {
+    $username = 'root';
+    $password = '';
+    $database ='test';
+    $hostname = 'localhost';
+}
+
+
